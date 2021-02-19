d62ec30bb76b89f48fc29e0a1c131bc205d6ff19
Your Name <suttonk@bcglobal.net>
2021-02-18 16:26:24 -0800

    added models and routes

---
+++ models/Exercise.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const ExerciseSchema = new Schema({
+    day: {
+        type: Date,
+        default: Date.now
+    },
+    exercises: [
+        {
+            type: {type: String},
+            name: {type: String},
+            duration: {type: Number},
+            weight: {type: Number},
+            reps: {type: Number};
+            sets: {type: Number};
+            distance: {type: Number};
+        }
+    ]
+});
+
+const Exercise = mongoose.model("Exercise", ExerciseSchema);
+
+module.exports = Exercise;
---
+++ servers.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/userdb", { useNewUrlParser: true });
+
+require("./routes/htmlRoutes.js")(app);
+require("./routes/apiRoutes.js")(app);
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
