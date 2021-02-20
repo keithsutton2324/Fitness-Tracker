6ded176b51ba9b9b82508b6fc77f08896350b9ae
Your Name <suttonk@bcglobal.net>
2021-02-19 20:00:08 -0800

    partial apiRoutes

--- servers.js
+++ servers.js
@@ -14,7 +14,7 @@ app.use(logger("dev"));
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/userdb", { useNewUrlParser: true });
 
 require("./routes/htmlRoutes.js")(app);
-require("./routes/apiRoutes.js")(app);
+//require("./routes/apiRoutes.js")(app);
 
 app.listen(PORT, function() {
   console.log(`Now listening on port: ${PORT}`);
